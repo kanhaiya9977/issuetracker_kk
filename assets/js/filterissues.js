4445b2d56b711e02eb7d968e4ebaeb0f95db1a49
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-05-08 16:19:22 +0530

    Update filterissues.js

--- assets/js/filterissues.js
+++ assets/js/filterissues.js
@@ -1,20 +1,31 @@
+// accessing form and divs from issue_form
 let filterIssueForm = document.getElementById('filter-issue-form');
 let issuesJson = document.getElementById('issue-data').getAttribute('data');
+
+//parssing issue data from div
 let issues = JSON.parse(issuesJson);
+
+//accessing issue lists from issues 
 let issueList = document.getElementById('issues-list');
+
 
 filterIssueForm.addEventListener('submit', function (e) {
   e.preventDefault();
 
   let filteredIssues = [];
 
+//   accessing labels by querySelector
   let labelsList = filterIssueForm.querySelectorAll('input[type=checkbox]');
+  
+  //adding multiple arrays of labels by '...' method
   let labelsElements = [...labelsList].filter((Element) => Element.checked);
 
+//   accessing authors
   let authorVal = filterIssueForm.querySelector(
     'input[type=radio][name=author]:checked'
   ).value;
 
+//   mapping and adding labels array
   let [...labelsArr] = labelsElements.map((Element) => Element.value);
 
   issues.map((el) => {
@@ -32,6 +43,7 @@ filterIssueForm.addEventListener('submit', function (e) {
     });
   });
   
+  //sending data from DOM to backend
   issueList.innerHTML = '';
   for (let issue of filteredIssues) {
     let Div = document.createElement('div');
