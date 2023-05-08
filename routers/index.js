2c6d8c77ac0c7c572419862c9c3baead54f1e6ea
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-05-08 16:34:51 +0530

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const homeController = require('../controllers/homw_controller');
 
+// routes for rendering home page
 router.get('/', homeController.home);
+
+//routes for accessing project file in which all neccessory routes defibed
 router.use('/project', require('./prject'))
 
 module.exports = router;
