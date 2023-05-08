dc4e0fd582852136d7887ecd4d292336cddec136
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-05-08 16:22:59 +0530

    Update homw_controller.js

--- controllers/homw_controller.js
+++ controllers/homw_controller.js
@@ -1,5 +1,6 @@
 const Project = require('../models/project');
 
+// Controller for fetching an sending projects datas from database to home page
 module.exports.home = async (req, res) => {
     try {
         let projects =await Project.find({}).sort('-createdAt');
